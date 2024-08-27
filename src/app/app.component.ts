68972557d8addb29982a2558005bcca099564e8a
RaphaelNunes10 <raphaelnunes10@outlook.com>
2024-08-27 15:40:40 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { CrudService } from './crud.service';
+import { Data } from './data.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-jsonserver-crud';
+  data: Data[];
+  dataString: string;
+  myText: string;
+
+  constructor(private crudService: CrudService) {
+    this.data = [];
+    this.dataString = "";
+    this.myText = "";
+  }
+
+  showData() {
+    this.crudService.getData().subscribe(data => {
+      this.data = data;
+      this.dataString = JSON.stringify(this.data);
+    })
+  }
+
+  addData(text: string) {
+    this.crudService.postData(text).subscribe(newData => {
+      console.log(JSON.stringify(newData));
+    })
+  }
+
+  ngOnInit() {
+    this.showData();
+  }
 }
---
+++ src/app/crud.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Data } from './data.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = "http://localhost:3000/data"
+  }
+
+  getData(): Observable<Data[]> {
+    return this.http.get<Data[]>(this.url)
+  }
+
+  postData(text: string): Observable<Data> {
+    return this.http.post<Data>(this.url, { text: text })
+  }
+}
